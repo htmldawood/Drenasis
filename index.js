65f12d4271d041acaecea028b0af3e4828cb0f48
htmldawood <retrolasd@gmail.com>
2025-12-18 16:47:12 +0000

    Revert "Its a fake tech company that i will be using as my portfolio for the future."
    This reverts commit 8f52d72cd76773520d1cba21daaff35a084c399b.

--- index.js
+++
@@ -1,8 +0,0 @@
-function toggle(elementId) {
-  var element = document.getElementById(elementId);
-  
-  if (element) {
-    // This is the simplest way: it flips the presence of the class
-    element.classList.remove('hide');
-  }
-}
